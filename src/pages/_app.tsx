b8a4ef45f363db1e48485d2e2f36b2c8075f3112
pacexy <pacexy@gmail.com>
2020-12-14 23:14:05 +0800

    fix: add types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import type { AppProps } from 'next/app'
+
+import React from 'react'
+
 import '@/styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
